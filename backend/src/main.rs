692fcce53b2b9c8d61b29cfe0a33255397f68d60
James Hawley II <iliketofeelcheesetootoo@yahoo.com>
2024-10-12 18:31:13 -0500

    Update dependency versions for backend, add rustfmt.toml file, and started to bring over what I learned from my practice-crud-operations. That being use dotenvy, and connect to the database.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,12 +4,28 @@ use math::add::add;
 use math::multiply::multiply;
 use quadratic_formula::quadratic_formula::QuadraticFormula;
 use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
-pub static PORT: &'static str = "3002";
+// pub static PORT: &'static str = "3002";
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world from backend.");
+    // dotenvy::dotenv().ok();
+    dotenvy::from_filename("/Users/jameshawley/MyCodingProjects/RustPractice/practice-data-processing/.env").ok();
+    // dotenvy::dotenv().expect("Failed to load .env file.");
+
+    let server_port = std::env::var("SERVER_PORT").expect("SERVER_PORT is not set in .env file.");
+    let server_address = std::env::var("SERVER_ADDRESS").expect("SERVER_ADDRESS is not set in .env file.");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file.");
+
+    let db_pool = PgPoolOptions::new()
+    .max_connections(16)
+    .connect(&database_url)
+    .await
+    .expect("Failed to connect to database.");
+
     println!("{}", add(2, 3));
     
     let qf = QuadraticFormula::new(1.0, -3.0, 2.0);
@@ -22,20 +38,23 @@ async fn main() {
 
     // let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     // http://127.0.0.1:3002
-    let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{}",PORT)).await.unwrap();
+    let listener = TcpListener::bind(format!("{}:{}", server_address, server_port))
+    .await
+    .expect("Failed to create TCP listener.");
     // axum::serve(listener, app).await.unwrap();
-    axum::serve(listener, create_router()).await.unwrap();
+    axum::serve(listener, create_router(db_pool)).await.unwrap();
 }
 
 async fn hello_world() -> String {
-    "Hello world_000000111".to_owned()
+    "Hello world_00000".to_owned()
 }
 
-pub fn create_router() -> Router {
+pub fn create_router(db_pool: Pool<Postgres>) -> Router {
     Router::new()
         .route("/", get(hello_world))
         // .route("createQuadraticFormula", get(create_quadratic_formula))
         // .route("getOneQuadraticFormula", get(get_one_quadratic_formula))
+        .with_state(db_pool)
 }
 
 
